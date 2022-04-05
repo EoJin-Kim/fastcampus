69494e25d4abcdd42cd86136191639ae6674f34d
김어진 <linexy@naver.com>
2022-04-06 01:04:33 +0900

    Feat: 백엔드 개발

--- src/main/java/com/example/fastcampus/order/dto/ResponseOrderDto.java
+++ src/main/java/com/example/fastcampus/order/dto/ResponseOrderDto.java
@@ -4,16 +4,24 @@ import com.example.fastcampus.order.entity.Order;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ResponseOrderDto {
 
     private Long shop;
     private LocalDateTime orderDate;
+    private List<String> foodList;
+    private Integer estimatedTime;
     public static ResponseOrderDto createResponseOrderDto(Order createOrder) {
         ResponseOrderDto responseOrderDto = new ResponseOrderDto();
         responseOrderDto.setShop(createOrder.getShop().getId());
+        ArrayList<String> foodList = new ArrayList<>();
         responseOrderDto.setOrderDate(createOrder.getOrderDate());
+        createOrder.getFoodList().stream()
+                .forEach(food -> foodList.add(food.getFoodName()));
+        responseOrderDto.setEstimatedTime(createOrder.getEstimatedTime());
         return responseOrderDto;
     }
 }
